dade34d785f82eb824e05326bbfca6f503a1ebd3
Liam44 <liam-b@orange.fr>
2017-05-08 11:41:27 +0200

    Changed line 10-15 and 18

--- GitForelOSD/Program.cs
+++ GitForelOSD/Program.cs
@@ -8,14 +8,14 @@ namespace GitForelOSD
 {
     class Program
     {
-           /// <summary>
-           /// My update /JB
-           /// </summary>
-           /// <param name="args"></param>
-           /// 
+        /// <summary>
+        /// My update /JB
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
         static void Main(string[] args)
         {
-            Console.WriteLine("TEst");
+            Console.WriteLine("Test"); // Changed TEst to Test
             Console.ReadKey();
         }
     }
