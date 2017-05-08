cffb2178dc9aa9ecff6e41795d901ab18e44a128
IcebearMAC <jocke@ny-flik.s>
2017-05-08 11:33:42 +0200

    Mn  COmmi

--- GitForelOSD/Program.cs
+++ GitForelOSD/Program.cs
@@ -8,8 +8,15 @@ namespace GitForelOSD
 {
     class Program
     {
+           /// <summary>
+           /// My update /JB
+           /// </summary>
+           /// <param name="args"></param>
+           /// 
         static void Main(string[] args)
         {
+            Console.WriteLine("TEst");
+            Console.ReadKey();
         }
     }
 }
